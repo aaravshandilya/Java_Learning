da6079a84e1a0c2edc836404c1684f7e6bdd2a6a
ashandilya1 <ashandilya1@pvschools.net>
2025-08-11 08:30:56 -0700

    Create week2Day4.java

---
+++ week2Day4.java
@@ -0,0 +1,32 @@
+public class week2Day4 {
+   public static void main(String[] args)  {
+        //System.out.println("Hello World!");
+        int $one = 5;
+        System.out.println($one);
+        String c = "25";
+        System.out.println(c);
+        double d = 5.0 / 2;
+        System.out.println(d);
+        
+        int a = 1;
+        System.out.println(a++);
+        System.out.println(++a);
+        System.out.println(a + 3 * 2);
+        
+        int x = -1;
+        x--;
+        System.out.println(x);
+        System.out.println(x--);
+        double y =  9/2;
+        System.out.println(y);
+
+        char ch = 'c';
+        System.out.println(ch);
+        int num = 8;
+        System.out.println(num);
+        System.out.println((char)(num));
+        System.out.println((int)ch);
+    }
+}
+
+
