1d0488df4ff363507cd668a72ff19fdb56ab25c4
aaravshandilya <aaravshandilya@gmail.com>
2025-08-11 17:09:50 -0700

    Update ProjectSTEM_Assignments.java

---
+++ Week2/ProjectSTEM_Assignments.java
@@ -0,0 +1,10 @@
+package Week2;
+import java.util.Scanner;
+public class ProjectSTEM_Assignments {
+    public static void main(String[] args) {
+        //Assignments for Project STEM Code here
+        Scanner scan = new Scanner(System.in);
+        scan.nextLine()
+        
+    }
+}
