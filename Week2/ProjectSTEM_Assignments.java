af2bf3c6cf255d5ba6fc130e4a1de4dfd9ae4f41
aaravshandilya <aaravshandilya@gmail.com>
2025-08-11 17:33:10 -0700

    Update ProjectSTEM_Assignments.java

--- Week2/ProjectSTEM_Assignments.java
+++ Week2/ProjectSTEM_Assignments.java
@@ -7,5 +7,20 @@ public class ProjectSTEM_Assignments {
         String food = scan.nextLine();
         System.out.println("I like to eat " + food + " as well!");
 
+        Scanner name1 = new Scanner(System.in);
+        System.out.println("Enter your name:");
+        String name = name1.nextLine();
+        String name2 = name1.nextLine();
+        String name3 = name1.nextLine();
+        String name4 = name1.nextLine();
+        System.out.println(name4 + " " + name2 + " " + name3 + " " + name );
+
+        Scanner nameScan = new Scanner(System.in);
+        System.out.println("Hi there! What is your name?");
+        String nameInput = nameScan.nextLine();
+        System.out.println("What State do you live in?");
+        String stateInput = nameScan.nextLine();
+        System.out.println("My name is " + nameInput + ". I live in " + stateInput + ".");
+
     }
 }
